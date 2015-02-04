8942543c314792332a4f7921ecb25bb101526c99
EvgenyAndrus <evg_an@mail.ru>
2015-02-05 01:27:43 +0300

    ready

---
+++ homework2/1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] mass = new int[10];
+            for (int i = 0; i < mass.Count(); i++)
+            {
+                mass[i] = rand.Next(0, 5);
+            }
+            Console.WriteLine("массив до сжатия");
+
+            for (int i = 0; i < mass.Count(); i++)
+            {
+                Console.Write(mass[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("массив после сжатия");
+            int countNull = 0;
+            for (int i = 0, j = 0; i < mass.Count(); i++)
+            {
+                if (mass[i] == 0)
+                {
+                    countNull++;
+                    j = i;
+                    while (mass[j] == 0)
+                    {
+                        if (j < mass.GetUpperBound(0)) j++;
+                        else break;
+                    }
+                    mass[i] = mass[j];
+                    mass[j] = 0;
+                }
+            }
+            if (countNull > 0)
+            {
+                for (int i = 0; i < mass.Count(); i++)
+                {
+                    if (mass[i] == 0) mass[i] = -1;
+                    Console.Write(mass[i] + " ");
+                }
+            }
+            if (countNull == 0)
+                Console.WriteLine("\nнет нулей в массиве");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework2/4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            /*Создание*/
+            int[,] mass = new int[3, 5];
+            /*Заполнение*/
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    mass[i, j] = rand.Next(0, 10);
+                }
+            }
+            /*вывод на экран*/
+            Console.WriteLine("Массив до смены столбцов\n");
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    Console.Write(mass[i, j] +" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("какие столбцы менять?");
+            int n, m;
+            n = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            /*Замена столбцов*/
+            int[,] tempMass = (int[,])mass.Clone();
+            //Console.WriteLine("n={0} m={1}",n,m);
+
+            
+                for (int i = 0; i < mass.GetLength(0); i++)
+                {
+                    mass[i, n] = tempMass[i, m];
+                    mass[i, m] = tempMass[i, n];                
+                }
+
+                /*вывод на экран после смены сталбцов*/
+                Console.WriteLine("Массив после смены столбцов\n");
+                for (int i = 0; i < mass.GetLength(0); i++)
+                {
+                    for (int j = 0; j < mass.GetLength(1); j++)
+                    {
+                        Console.Write(mass[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            
+            Console.Read();
+
+        }
+    }
+}

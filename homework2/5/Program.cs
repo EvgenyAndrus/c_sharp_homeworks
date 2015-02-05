60fce4746d1e01c8b67096f7248c8e1c0043d29f
EvgenyAndrus <evg_an@mail.ru>
2015-02-05 12:37:12 +0300

    modified. ready

--- homework2/5/Program.cs
+++ homework2/5/Program.cs
@@ -8,50 +8,46 @@ namespace program
 {
     class Program
     {
-       static void InfoStr(string str)
+        static void InfoStr(string str)
         {
-           /*Общее количество символов*/
+            /*Общее количество символов*/
             Console.WriteLine("Общее количество символов: {0}", str.Count());
-           /*В верхнем и нижнем регистре*/
+            /*В верхнем и нижнем регистре*/
             int upperRegister = 0;
             int lowerRegister = 0;
             int numbDigits = 0;
             int numSpaces = 0;
             int punctuation = 0;
-            for (int i = 0; i < str.Count();i++ )
+            foreach (char i in str)
             {
-                if (str[i] >= 'A' && str[i] <= 'Z')
+                if (i >= 'A' && i <= 'Z')
                 {
                     upperRegister++;
                 }
-                else if (str[i] >= 'a' && str[i] <= 'z')
+                else if (i >= 'a' && i <= 'z')
                 {
                     lowerRegister++;
                 }
                 /*количество цифр*/
-                else if (str[i] >= '0' && str[i] <= '9')
+                else if (i >= '0' && i <= '9')
                 {
                     numbDigits++;
                 }
                 /*количество пробелов*/
-                else if (str[i] == ' ')
+                else if (i == ' ')
                 {
                     numSpaces++;
                 }
                 else punctuation = str.Count() - (upperRegister + lowerRegister + numbDigits + numSpaces);
             }
-            Console.WriteLine("Количество символов в верхнем регистре: {0}\nКоличество символов нижнем регистре: {1}\nКоличество цифр: {2}\nКоличество пробелов: {3}\nКоличество символов пунктуации: {4}", upperRegister, lowerRegister, numbDigits, numSpaces,punctuation);
-
+            Console.WriteLine("Количество символов в верхнем регистре: {0}\nКоличество символов нижнем регистре: {1}\nКоличество цифр: {2}\nКоличество пробелов: {3}\nКоличество символов пунктуации: {4}", upperRegister, lowerRegister, numbDigits, numSpaces, punctuation);
         }
         static void Main(string[] args)
         {
             Console.Write("Введи строку: ");
-            string str=Console.ReadLine();
+            string str = Console.ReadLine();
             InfoStr(str);
-
-
             Console.Read();
         }
-       
     }
 }

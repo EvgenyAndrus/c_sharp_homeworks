1f6d5a1f3aeb15e6f520b586634953b3dcfb2f1c
EvgenyAndrus <evg_an@mail.ru>
2015-02-10 22:35:12 +0300

    Not ready

---
+++ homework3/homework3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите А и В через запятую или пробел");
+            try
+            {
+                StructType.Parse(Console.ReadLine());
+            }
+            catch (FormatException exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("{0} * X + {1} * Y = 0", StructType.a, StructType.b);
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ homework3/homework3/StructType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3
+{
+    static class StructType
+    {
+        internal static int a { get; set; }
+        internal static int b { get; set; }
+        internal static void Parse(string str)
+        {
+            string[] split = str.Split(",".ToCharArray());
+            if (split.Length != 2)
+                split = str.Split(" ".ToCharArray());
+            if (split.Length != 2)
+                throw new FormatException();
+            try
+            {
+                a = int.Parse(split[0]);
+                b = int.Parse(split[1]);
+            }
+            catch (FormatException)
+            {
+                throw new FormatException();
+            }
+        }
+    }
+}
